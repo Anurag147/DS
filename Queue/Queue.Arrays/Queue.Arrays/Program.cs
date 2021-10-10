073c76a778e665a7ef0bb3d387b1669a2ce81638
Anurag <a.asthana90@gmail.com>
2021-10-10 18:48:12 +0530

    Queue DS

---
+++ Queue/Queue.Arrays/Queue.Arrays/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Queue.Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QueueArray<int> qrr = new QueueArray<int>();
+
+            Console.WriteLine("************************Adding Items******************");
+            qrr.Enqueue(4);
+            qrr.Enqueue(3);
+            qrr.Enqueue(2);
+            qrr.Enqueue(1);
+            qrr.Enqueue(0);
+            qrr.Enqueue(9);
+            qrr.Enqueue(10);
+
+            Console.WriteLine("************************Removing Items******************");
+            Console.WriteLine(qrr.DeQueue());
+            Console.WriteLine(qrr.DeQueue());
+            Console.WriteLine(qrr.DeQueue());
+            Console.WriteLine(qrr.DeQueue());
+            Console.WriteLine(qrr.DeQueue());
+
+            Console.WriteLine("************************Showing Items******************");
+            foreach (int item in qrr)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Queue/Queue.Arrays/Queue.Arrays/QueueArray.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Queue.Arrays
+{
+    public class QueueArray<T>:IEnumerable<T>
+    {
+        public T[] Queue { get; set; }
+        public int Head { get; set; }
+        public int Tail { get; set; }
+        public int ArrayLength = 5;
+        public QueueArray()
+        {
+            Queue = new T[ArrayLength];
+        }
+        public void Enqueue(T obj)
+        {
+            if (Queue.Length == ArrayLength)
+            {
+                T[] newQueue = new T[ArrayLength * 2];
+                Array.Copy(Queue, newQueue, ArrayLength);
+                Queue = newQueue;
+                ArrayLength *= 2;
+            }
+            Queue[Tail] = obj;
+            Tail++;
+        }
+        public T DeQueue()
+        {
+            if (Tail-Head==0)
+            {
+                throw new Exception("The queue is already empty");
+            }
+            T dequeuedObj = Queue[Head];
+            Head++;
+            return dequeuedObj;
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            for(int i = Head; i < Tail; i++)
+            {
+                yield return Queue[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
